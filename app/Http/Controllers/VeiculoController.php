cc56494a1441bcfa23f7acf62f2f966f3dc9bc39
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2017-03-12 22:04:56 -0300

    API quase pronta

---
+++ app/Http/Controllers/VeiculoController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Veiculo;
+
+class VeiculoController extends Controller
+{
+
+    protected function veiculoValidator($request){
+        $validator = Validator::make($request->all(),[
+            'marca' => 'required',
+            'modelo' => 'required',
+            'cor' => 'required',
+            'ano' => 'required',
+            'preco' => 'required|numeric|min:0',
+            ]);
+        return $validator;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $veiculo = Veiculo::all();
+        return response()->json(['data'=>$veiculo, 'status'=>true]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = $this->veiculoValidator($request);
+        if($validator->fails()){
+            return response()->json(['response'=>'Erro', 
+                'errors' => $validator->errors()], 
+                400);
+        }
+        $data = $request->only(['marca', 'modelo', 'cor', 'ano', 'preco']);
+        if($data){
+            $veiculo = Veiculo::create($data);
+            if($veiculo){
+                return response()->json(['response'=>'Veículo criado com sucesso', 'data'=> $veiculo], 201);
+            }else{
+                return response()->json(['response'=>'Erro ao criar o veículo', 'data'=> $veiculo], 201);
+            }
+        }else{
+            return response()->json(['response'=>'Dados inválidos'], 400);
+        }        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if($id < 0){
+            return response()->json(['response'=>'ID menor que zero'], 400);
+        }
+        $veiculo = Veiculo::find($id);
+        if($veiculo){
+            return response()->json(['data'=> $veiculo], 200);
+        }else{
+            return response()->json(['response'=>'O veículo com id '.$id.' não existe'], 404);
+        }
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = $this->veiculoValidator($request);
+        if($validator->fails()){
+            return response()->json(['response'=>'Erro', 
+                'errors' => $validator->errors()], 
+                400);
+        }
+        $data = $request->only(['marca', 'modelo', 'cor', 'ano', 'preco']);
+        if($data){
+            $veiculo = Veiculo::find($id);
+            if($veiculo){
+                $veiculo->update($data);
+                return response()->json(['response'=>'Veículo atualizado com sucesso', 'data'=> $veiculo], 200);
+            }else{
+                return response()->json(['response'=>'O veículo com id '.$id.' não existe'], 400);
+            }
+        }else{
+            return response()->json(['response'=>'Dados inválidos'], 400);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if($id < 0){
+            return response()->json(['response'=>'ID menor que zero'], 400);
+        }
+        $veiculo = Veiculo::find($id);
+        if($veiculo){
+            $veiculo->delete();
+            return response()->json(['response'=>'Veículo removido com sucesso'], 200);
+        }else{
+            return response()->json(['response'=>'O veículo com id '.$id.' não existe'], 404);
+        }
+    }
+    
+}
---
+++ database/migrations/2017_03_12_195732_create_veiculos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVeiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('veiculos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->string('cor');
+            $table->integer('ano');
+            $table->float('preco');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('veiculos');
+    }
+}
